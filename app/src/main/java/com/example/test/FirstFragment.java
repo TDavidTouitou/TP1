161dfb012f3cd9a16cb7c2dd085d5cb31a1f9917
TDavidTouitou <touitou@et.esiea.fr>
2020-03-19 18:46:26 +0100

    Partie 8 finie

--- app/src/main/java/com/example/test/FirstFragment.java
+++ app/src/main/java/com/example/test/FirstFragment.java
@@ -23,6 +23,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
